44fb71172399d023f16979dc97b9f65e29fd9a99
JuanIgnacioOchoa <juanignacio8ag@gmail.com>
2017-10-24 10:38:28 -0500

    a

--- clientChat.c
+++ clientChat.c
@@ -4,67 +4,93 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define NICK_SIZE 20
+#define STR_SIZE 40
 
 void *writeS(void *arg);
 void *readS(void *arg);
+
+// OH GOD YES
 int sockfd;
 pthread_t tid[2];
+
 int main()
 {
-    
     int len;
     struct sockaddr_in address;
+
     int result;
-    char* ch = 'A';
+    char *ch = 'A';
+
     /* Crear un socket para el cliente */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
     /* Nombrar el socket, de acuerdo con el server */
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
     address.sin_port = htons(9734);
     len = sizeof(address);
     result = connect(sockfd, (struct sockaddr *)&address, len);
+
     if (result == -1)
     {
-        perror("oops: client");
+        perror("Oops: client");
         exit(1);
     }
+    char name[NICK_SIZE];
+    int name_size = (int)(sizeof(name)*sizeof(char));
+
     printf("Ingresar nombre de usuario(Max 20 caracteres)\n");
-    scanf("%s", &ch);
-    printf("Welcome %c\n", ch);
-    write(sockfd, &ch, 1);
+
+    fgets(name, name_size, stdin);
+    printf("Welcome %s_________________\n", name);
+
+    write(sockfd, &name, name_size);
+
     pthread_create(&tid[0], NULL, writeS, (void *)&ch);
     pthread_create(&tid[1], NULL, readS, (void *)&ch);
-    while(1){
-        sleep(5);
+
+    int i;
+    for (i = 0; i < 2; i++)
+    {
+        pthread_join(tid[i], NULL);
     }
+
+    free(ch);
+
     exit(0);
 }
 void *writeS(void *arg)
 {
-    char *ch;
+    char str[STR_SIZE];
     //printf("start writing thread\n");
-    while(1) {
+    while (1)
+    {
         //printf("scanning\n");
-        scanf("%s", &ch);
-        //printf("Write %s\n",&ch);
-        write(sockfd, &ch, 1);
+        fgets(str, STR_SIZE, stdin);
+        //printf("%s\n", str);
+        write(sockfd, &str, sizeof(str));
         sleep(1);
     }
-    printf("bye\n");
-    return;
+    free(str);
+    //return;
 }
 void *readS(void *arg)
 {
     //printf("start reading thread\n");
-    char *ch;
-    int  *n = (int *)arg;
+    char str[STR_SIZE];
+    int *n = (int *)arg;
     int mynum = *n;
-    while(1) {
-        read(sockfd, &ch,   1);
-        printf("reading %s\n", &ch);
+    while (1)
+    {
+        read(sockfd, &str, sizeof(str));
+        printf("%s\n", str);
         sleep(1);
     }
-
-    return;
+    free(str);
+    free(n);
+    //return;
 }
--- serverChat.c
+++ serverChat.c
@@ -5,14 +5,18 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_CLIENT 10
-
+#define NICK_SIZE 20
+#define STR_SIZE 40
 
 int client_sockfd[MAX_CLIENT];
 void *coneccion_clte(void *arg);
-char user[10][20];
+char user[10][NICK_SIZE];
 int totcltes = 0;
+
 int main()
 {
     int server_sockfd;
@@ -21,53 +25,76 @@ int main()
     struct sockaddr_in client_address;
     int parametro[MAX_CLIENT];
     pthread_t tid[MAX_CLIENT];
+
     /* Crear un socket sin nombre para el server */
     server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
     /* Nombrar el socket */
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
     server_address.sin_port = htons(9734);
     server_len = sizeof(server_address);
+
     bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
     listen(server_sockfd, 5);
-    printf("Server started\n");
+
+    printf("Server started!\n");
+
     while (totcltes < MAX_CLIENT)
     {
-        printf("clientes\n");
+        //printf("clientes\n");
         char ch;
         client_sockfd[totcltes] = accept(server_sockfd,
-                               (struct sockaddr *)&client_address, &client_len);
+                                         (struct sockaddr *)&client_address, &client_len);
         parametro[totcltes] = totcltes;
         pthread_create(&tid[totcltes], NULL, coneccion_clte, (void *)&parametro[totcltes]);
-        totcltes ++;
+        totcltes++;
     }
-    printf("Maximo de clientes alcanzado\n");
+
+    printf("Maximo de clientes alcanzado!\n");
+    return 0;
 }
 void *coneccion_clte(void *arg)
 {
-    
-    char *ch;
-    int  *n = (int *)arg;
-    int mynum = *n;
+    char str[STR_SIZE];
+    int *n = (int *)arg;
+    int mynum = *n; 
     int i = 0;
-    read(client_sockfd[mynum], &ch, 1);
-    printf("El usuario %s ha ingresado al chat mynum %d\n",&ch, mynum);
-    *user[mynum] = ch;
-    while(1 == 1) {
-        printf("reading\n");
-        read(client_sockfd[mynum], &ch, 1);
+
+    read(client_sockfd[mynum], &str, sizeof(str));
+    printf("El usuario %s ha ingresado al chat %d\n", str, mynum);
+    
+    //user[mynum][0] = "asd";
+    memset(user[mynum], '\0', sizeof(user[mynum]));
+    strcpy(user[mynum], str);
+
+    while (1)
+    {
+        
+        //printf("reading\n");
+        read(client_sockfd[mynum], &str, sizeof(str));
         i = 0;
-        while(i < totcltes)
+        while (i < totcltes)
         {
-            //printf("writing %s to user %s\n",&ch, &user[i]);
-           if(i != mynum){
-                printf("writing %s to user %s\n",&ch, &user[i]);
-                write(client_sockfd[i], &ch, 1);
-           }
+            
+            //printf("writing %s to user %s\n",&str, &user[i]);
+            if (i != mynum)
+            {
+                printf("Writing %s to user %s...\n", str, user[i]);
+                char message[STR_SIZE+NICK_SIZE+2];
+                memset(message, '\0', sizeof(message));
+                strncat(message, user[mynum], sizeof(user[mynum]));
+                strcat(message, "\t");
+                strcat(message, str);
+                write(client_sockfd[i], &message, sizeof(message));
+            }
             i++;
         }
         sleep(1);
     }
 
-    return;
+    free(str);
+    free(n);
+
+    //return;
 }
